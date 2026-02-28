00f8cd96a8ae3966c490a617c636ef801db8eb6b
Eljazou <eljazouly.abdessamad2003@gmail.com>
2026-02-28 15:04:32 +0000

    Initial commit

---
+++ src/test/java/com/example/charite/ChariteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.charite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChariteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
