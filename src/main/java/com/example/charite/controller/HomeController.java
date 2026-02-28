5052ceb5186b50ae0fe8815df80d73a6af974114
Eljazou <eljazouly.abdessamad2003@gmail.com>
2026-02-28 16:20:44 +0000

    db+security

--- src/main/java/com/example/charite/controller/HomeController.java
+++ src/main/java/com/example/charite/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.charite.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
 }
---
+++ src/main/java/com/example/charite/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.charite.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
