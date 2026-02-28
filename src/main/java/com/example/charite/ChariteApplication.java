3ebaf54b82f1ca718eea4869229501a7a8223e74
Eljazou <eljazouly.abdessamad2003@gmail.com>
2026-02-28 15:04:32 +0000

    Initial commit

---
+++ src/main/java/com/example/charite/ChariteApplication.java
@@ -0,0 +1,13 @@
+package com.example.charite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChariteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChariteApplication.class, args);
+	}
+
+}
